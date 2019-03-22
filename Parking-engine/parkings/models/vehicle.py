de9492cadf03c79e2832ab75a082f4cd3aeb34bb
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-03-23 03:20:53 +0800

    Models - Car create class Car

---
+++ Parking-engine/parkings/models/vehicle.py
@@ -0,0 +1,8 @@
+class Car(object):
+    """Car Vehicle for filling the slots in parking lot.
+    """
+
+    def __init__(self, regnum, color):
+        # Initialise Car information
+        self._regnum = regnum
+        self._color = color
