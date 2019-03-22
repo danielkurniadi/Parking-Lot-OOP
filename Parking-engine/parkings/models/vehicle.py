ecf2597bdae36a8db1fa7dd05cd9719a5ff827a6
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-03-23 03:21:43 +0800

    Models - Car implement properties getters

--- Parking-engine/parkings/models/vehicle.py
+++ Parking-engine/parkings/models/vehicle.py
@@ -6,3 +6,16 @@ class Car(object):
         # Initialise Car information
         self._regnum = regnum
         self._color = color
+
+    @property
+    def regnum(self):
+        if not hasattr(self, "_regnum"):
+            self._regnum = "(REGNUM NOT DEFINED)"
+        return self._regnum
+
+    @property
+    def color(self):
+        if not hasattr(self, "_color"):
+            self._color = "(COLOR NOT DEFINED)"
+        return self._color
+
