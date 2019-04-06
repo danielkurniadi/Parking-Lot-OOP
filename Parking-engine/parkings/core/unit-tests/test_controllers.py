94d635cae6b750c985834369ae6ec3eb6beae655
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-04-06 14:00:00 +0800

    Core - Controllers setup unit test

---
+++ Parking-engine/parkings/core/unit-tests/test_controllers.py
@@ -0,0 +1,48 @@
+import unittest
+
+from parkings.core.commands import Command
+from parkings.core.controllers import Controller
+from parkings.models.vehicle import Car
+from parkings.models.spaces import CarSlots
+
+class ControllerTests(unittest.TestCase):
+    """Tests case for controller class
+    """
+
+    def setUp(self):
+        """Setup is called every test
+        """
+        self.size = 5
+        self.parking_lot = CarSlots(self.size)
+        self.controller = Controller(self.parking_lot)
+
+    def prepare_empty_lot(self, n):
+        """Helper function for setup purpose. Prepare empty lot
+        """
+        start_new_cmd = "create_parking_lot"
+        return self.controller.execute(start_new_cmd, *(n,))
+
+    def prepare_cars(self, n):
+        """Helper function for setup purpose. Park multiple cars
+        """
+        # Setup args
+        park_cmd = "park"
+        cars_args = [("Regnum-{}".format(idx), "Color-{}".format(idx)) 
+            for idx in range(1, n+1)]
+
+        # Prepare an empty parking lot
+        self.prepare_empty_lot(n)
+
+        # Park multiple cars
+        for arg in cars_args:
+            self.controller.execute(park_cmd, *arg)
+        return cars_args
+
+    def tearDown(self):
+        # tearDown is called every test methods in this class
+        self.parking_lot.wipe_all()
+
+    #############################################################################
+
+
+
