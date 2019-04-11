6ff3d5225b0eb1187a3503d9fa8dd94ecb239543
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-04-11 14:00:00 +0800

    Core - Controller implement unit tests on execute status & queries

--- Parking-engine/parkings/core/unit-tests/test_controllers.py
+++ Parking-engine/parkings/core/unit-tests/test_controllers.py
@@ -5,8 +5,9 @@ from parkings.core.controllers import Controller
 from parkings.models.vehicle import Car
 from parkings.models.spaces import CarSlots
 
-class ControllerTests(unittest.TestCase):
-    """Tests case for controller class
+class ControllerExecuteTests(unittest.TestCase):
+    """Base Test Case for controller class with abstract method for setup
+    sub-testcase class.
     """
 
     def setUp(self):
@@ -45,7 +46,7 @@ class ControllerTests(unittest.TestCase):
     #############################################################################
 
     def test_execute_start_new(self):
-        """Command object is able to execute start new parking lot.
+        """Controller is able to execute start new parking lot.
         """
         # Setup params
         n_slots = 10
@@ -57,7 +58,7 @@ class ControllerTests(unittest.TestCase):
         self.assertEqual(output, n_slots)
 
     def test_execute_park_car(self):
-        """Command object is able to execute park car 
+        """Controller is able to execute park car 
         """
         # Setup params
         n_slots = 2
@@ -78,7 +79,7 @@ class ControllerTests(unittest.TestCase):
         self.assertEqual(slot_id, 2)
 
     def test_execute_leave_car(self):
-        """Command object is able to execute delete/leave car command.
+        """Controller object is able to execute delete/leave car command.
         """
         # Setup params
         n_slots = 3
@@ -90,5 +91,36 @@ class ControllerTests(unittest.TestCase):
             slot_id = self.controller.execute(leave_cmd, *(i,))
         
         # Verify parking lot is empty
-        car_count = self.parking_lot.count_cars()
+        car_count = self.parking_lot.count_vehicle()
         self.assertEqual(car_count, 0)
+
+    def test_execute_status(self):
+        """Controller object is able to execute status command.
+        """
+        # Setup params
+        n_slots = 3
+        status_cmd = "status"
+
+        # Prepare n number of empty slots
+        self.prepare_cars(n_slots)
+
+        # Verify command is able to execute without errors
+        self.controller.execute(status_cmd)
+
+    def test_execute_queries(self):
+        """Controller object is able to execute queries command.
+        """
+        # Setup params
+        n_slots = 5
+        query_regnums_by_color = "registration_numbers_for_cars_with_colour"
+        query_slotids_by_color = "slot_number_for_registration_number"
+        query_slotid_by_regnum = "slot_numbers_for_cars_with_colour"
+        commands = [
+            (query_regnums_by_color, "Color-2"),
+            (query_slotids_by_color, "Color-1"),
+            (query_slotid_by_regnum, "Renum-5")
+        ]
+
+        # Verify command is able to execute without errors
+        for query, arg  in commands:
+            self.controller.execute(query, *(arg,))
