c665759248b80f8189fa06428b7f9a806aa73166
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-04-11 14:00:00 +0800

    Core - Command create enumerator class for cmd input

---
+++ Parking-engine/parkings/core/commands.py
@@ -0,0 +1,16 @@
+
+class Command():
+    """Command wrapper for command input. This wrapper unifies all input, 
+    be it from interactive session or text file input. Command wrapper
+    serve as core controller that interact with the models.
+    """
+ 
+    # enumerate available command types. 
+    # add and command list here for extention.
+    CREATE = "create_parking_lot"
+    PARK = "park"
+    LEAVE = "leave"
+    STATUS = "status"
+    QUERY_REGNUMS_BY_COLOR = "registration_numbers_for_cars_with_colour"
+    QUERY_SLOTNUM_BY_REGNUM = "slot_number_for_registration_number"
+    QUERY_SLOTNUMS_BY_COLOR = "slot_numbers_for_cars_with_colour"
